473fb82b703708b94d4956c72d70251be297f7ea
vim <vim@42urduliz.com>
2022-04-03 01:20:19 +0200

    update(parser): Argument processing and leak removing

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -1,15 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vim <vim@42urduliz.com>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 22:15:08 by vim               #+#    #+#             */
+/*   Updated: 2022/04/03 01:14:10 by vim              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <command.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <libft.h>
 
 t_command	*parser_parse(char *line)
 {
 	t_command	*commands;
 
 	commands = command_splitter(line);
-	command_iterator(commands, command_replace_vars); 
-	command_iterator(commands, command_redirections); 
-
+	command_iterator(commands, command_replace_vars);
+	command_iterator(commands, command_redirections);
+	command_iterator(commands, command_do_arguments);
 	return (commands);
 }
 
@@ -24,4 +37,5 @@ void	parser_free(t_command *commands)
 		close(commands->fdin);
 	if (commands->fdout != 0)
 		close(commands->fdout);
+	free(commands);
 }
