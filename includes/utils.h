eda17d65df75ef29c0206386d712635e64b636d8
vim <vim@42urduliz.com>
2022-04-29 01:48:56 +0200

    fix: Fixed variable updating

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: vim <vim@42urduliz.com>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 23:28:00 by vim               #+#    #+#             */
-/*   Updated: 2022/04/25 16:42:03 by mortega-         ###   ########.fr       */
+/*   Updated: 2022/04/29 01:36:15 by mmartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # define DELETE -1
 
 # include <stdbool.h>
+# include <command.h>
 
 typedef bool	(*t_validator)(char const);
 
@@ -30,6 +31,9 @@ char	*utils_strpush(char *str, char const c);
 ** Checks for \t and ' '
 */
 bool	utils_validator_isspace(char const c);
+
+bool	utils_validator_command_line(t_command *cmd);
+
 /*
 ** Iterates the string until t_validator returns false
 */
