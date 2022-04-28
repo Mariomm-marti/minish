e2579b70b67c2619ea7371cb0911e87f520dd324
vim <vim@42urduliz.com>
2022-04-29 01:48:36 +0200

    wip: Signals half way

--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: mortega- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 19:11:00 by mortega-          #+#    #+#             */
-/*   Updated: 2022/04/28 22:39:41 by mmartin-         ###   ########.fr       */
+/*   Updated: 2022/04/29 01:26:26 by mmartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <utils.h>
 #include <libft.h>
 #include <utils.h>
 
@@ -23,12 +24,7 @@ void	handler(int sig)
 	if (sig == SIGINT)
 	{
 		// all-free function
-		rl_redisplay();
-		printf("\r\n");
-		//write(1, "\n", 1);
-		//write(1, show_prompt(), ft_strlen(show_prompt()));
-		//exit(130);
+		printf("\r\nminiSH$ ");
+		utils_update_var("?", "130");
 	}
-	else if (sig == SIGQUIT)
-		;
 }
