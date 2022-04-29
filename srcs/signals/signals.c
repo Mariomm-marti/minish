7f605d23245ba5822dc6183bc1b64048b31499ba
vim <vim@42urduliz.com>
2022-04-29 03:23:42 +0200

    update: Given consistency to signal handling

--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: mortega- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 19:11:00 by mortega-          #+#    #+#             */
-/*   Updated: 2022/04/29 03:07:16 by mmartin-         ###   ########.fr       */
+/*   Updated: 2022/04/29 03:21:06 by mmartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,10 @@
 #include <libft.h>
 #include <utils.h>
 
-//TODO all-free function
 void	handler(int sig)
 {
-	if (sig == SIGINT)
-	{
-		printf("\r\nminiSH$ ");
-		utils_update_var("?", "130");
-	}
+//TODO all-free function
+	(void)sig;
+	rl_redisplay();
+	utils_update_var("?", "130");
 }
