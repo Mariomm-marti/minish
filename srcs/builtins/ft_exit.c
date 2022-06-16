668492763c91e9adaec080652d472e6eace4d83a
Test 42 <test@c1r15s5.42madrid.com>
2022-06-16 21:48:58 +0200

    fixed: exit

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: mortega- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 16:48:28 by mortega-          #+#    #+#             */
-/*   Updated: 2022/06/16 21:30:54 by test             ###   ########.fr       */
+/*   Updated: 2022/06/16 21:47:20 by test             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ ssize_t	ft_exit(char const **argv, int fdin, int fdout)
 	(void)fdin;
 	(void)fdout;
 	free_environ();
-	write(1, "exit", 4);
+	write(1, "exit\n", 5);
 	if (*(argv + 1))
 		exit(ft_atoi(*(argv + 1)));
 	exit(code);
